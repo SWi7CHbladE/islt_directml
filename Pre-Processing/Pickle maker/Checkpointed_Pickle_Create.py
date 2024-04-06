328977f98a1c7016a277f52c85ec186ff2f525c7
Rahul Gogoi <41523631+SWi7CHbladE@users.noreply.github.com>
2024-04-06 16:33:22 +0530

    Update Checkpointed_Pickle_Create.py

--- Pre-Processing/Pickle maker/Checkpointed_Pickle_Create.py
+++ Pre-Processing/Pickle maker/Checkpointed_Pickle_Create.py
@@ -21,12 +21,9 @@ import platform
 from progress.bar import Bar
 
 # Function to save checkpoint
-def save_checkpoint(checkpoint_path, data, count):
-    checkpoint_dir = os.path.dirname(checkpoint_path)
-    if not os.path.exists(checkpoint_dir):
-        os.makedirs(checkpoint_dir)
+def save_checkpoint(checkpoint_path, list_of_inputs):
     with open(checkpoint_path, 'wb') as f:
-        pickle.dump({'data': data, 'count': count}, f)
+        pickle.dump(list_of_inputs, f)
 
 # Function to load checkpoint
 def load_checkpoint(checkpoint_path):
@@ -36,20 +33,14 @@ def load_checkpoint(checkpoint_path):
     else:
         return None
 
-# ***Function for extraction of features***
-def get_features(filename, destination, checkpoint_path):
-    checkpoint_data = load_checkpoint(checkpoint_path)
-    checkpoint_count = checkpoint_data['count'] if checkpoint_data else 0
-    processed_data = checkpoint_data['data'] if checkpoint_data else []
-    
+# Function for extraction of features
+def get_features(filename, destination):
     input_string = filename
     pattern = r'\d+'
     match = re.search(pattern, input_string)
-    
     if match:
         first_match = match.group()
         input_folder = os.path.join(os.getcwd(), destination, first_match, input_string)
-        
         try:
             file_paths_frames = [file for file in sorted(os.listdir(input_folder)) if file.endswith(".jpg")]
         except:
@@ -59,7 +50,7 @@ def get_features(filename, destination, checkpoint_path):
         x = base_model.output
         x = GlobalAveragePooling2D()(x)
         feature_extractor = Model(inputs=base_model.input, outputs=x)
-    
+
         features_listofList = []
         for indx, frame_file in enumerate(file_paths_frames):
             frame_filename = os.path.join(input_folder, frame_file)
@@ -69,16 +60,9 @@ def get_features(filename, destination, checkpoint_path):
             image = np.expand_dims(image, axis=0)
             spatial_embedding = feature_extractor.predict(image)[0]
             features_listofList.append(spatial_embedding)
-            checkpoint_count += 1
-            processed_data.append({'filename': frame_file, 'features': spatial_embedding})
-            if checkpoint_count % 10 == 0:
-                print("Checkpoint created:", checkpoint_count)
-                save_checkpoint(checkpoint_path, processed_data, checkpoint_count)
-
-        save_checkpoint(checkpoint_path, processed_data, checkpoint_count)
         return torch.tensor(features_listofList)
     else:
-        print("No match found.")
+        print("No match found for: " + input_string + "\n")
         return None
 
 # Function to create the pickle file
@@ -88,21 +72,35 @@ def create_pickle(workbook_dest, output_dest, frame_dest, checkpoint_path):
     excel_data = []
     for row in sheet.iter_rows(values_only=True):
         excel_data.append(row)
-    
-    list_of_inputs = []
-    for tmp in excel_data:
-        features = get_features(str(tmp[0]), frame_dest, checkpoint_path)
-        if features is not None:
-            if len(features) > 0:
-                data_dict = {
-                    'name': tmp[0],
-                    'signer': tmp[1],
-                    'gloss': tmp[2],
-                    'text': tmp[3],
-                    'sign': features + 1e-8
-                }
-                list_of_inputs.append(data_dict)
-    
+
+    # Load checkpoint
+    list_of_inputs = load_checkpoint(checkpoint_path)
+    if list_of_inputs is None:
+        list_of_inputs = []
+
+    # Get the features
+    for index in range(len(list_of_inputs), len(excel_data), 10):
+        batch_list_of_inputs = []
+        for tmp in excel_data[index:index+10]:
+            features = get_features(str(tmp[0]), frame_dest)
+            if features is not None:
+                if len(features) > 0:
+                    data_dict = {
+                        'name': tmp[0],
+                        'signer': tmp[1],
+                        'gloss': tmp[2],
+                        'text': tmp[3],
+                        'sign': features + 1e-8
+                    }
+                    batch_list_of_inputs.append(data_dict)
+        
+        # Update list_of_inputs
+        list_of_inputs.extend(batch_list_of_inputs)
+
+        # Save checkpoint
+        save_checkpoint(checkpoint_path, list_of_inputs)
+
+    # Save final pickle file
     with gzip.open(os.path.join(os.getcwd(), output_dest), 'wb') as f:
         pickle.dump(list_of_inputs, f)
 
@@ -110,23 +108,22 @@ def create_pickle(workbook_dest, output_dest, frame_dest, checkpoint_path):
 vw_dest = "Dataset/excels/Validation.xlsx"
 vo_dest = "Dataset/Pickles/excel_data.dev"
 vf_dest = "Dataset/Final folder for frames"
+dev_checkpoint_path = 'Dataset/dev_checkpoint.pkl'
+
+create_pickle(vw_dest, vo_dest, vf_dest, dev_checkpoint_path)
 
 tw_dest = "Dataset/excels/Test.xlsx"
 to_dest = "Dataset/Pickles/excel_data.test"
 tf_dest = "Dataset/Final folder for frames"
+test_checkpoint_path = 'Dataset/test_checkpoint.pkl'
+
+create_pickle(tw_dest, to_dest, tf_dest, dev_checkpoint_path)
 
 w_dest = "Dataset/excels/Train.xlsx"
 o_dest = "Dataset/Pickles/excel_data.train"
 f_dest = "Dataset/Final folder for frames"
-
-# Checkpoint paths
-dev_checkpoint_path = 'Dataset/dev_checkpoint.pkl'
-test_checkpoint_path = 'Dataset/test_checkpoint.pkl'
 train_checkpoint_path = 'Dataset/train_checkpoint.pkl'
 
-# Create pickles
-create_pickle(vw_dest, vo_dest, vf_dest, dev_checkpoint_path)
-create_pickle(tw_dest, to_dest, tf_dest, test_checkpoint_path)
 create_pickle(w_dest, o_dest, f_dest, train_checkpoint_path)
 
 print("Done creating pickle files.")
