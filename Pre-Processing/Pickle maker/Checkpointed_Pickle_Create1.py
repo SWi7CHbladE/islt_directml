2e8d675f0b085b7f547174bcaa25c3c67adc96c8
Rahul Gogoi <41523631+SWi7CHbladE@users.noreply.github.com>
2024-04-24 00:19:40 +0530

    Solved memory hogging V1

--- Pre-Processing/Pickle maker/Checkpointed_Pickle_Create1.py
+++ Pre-Processing/Pickle maker/Checkpointed_Pickle_Create1.py
@@ -19,6 +19,7 @@ from tensorflow.keras import mixed_precision
 from openpyxl import Workbook, load_workbook
 import platform
 from progress.bar import Bar
+tf.config.run_functions_eagerly = True
 
 print("***\nCurrent working directory:\n")
 print(os.getcwd())
@@ -43,6 +44,13 @@ def load_checkpoint(checkpoint_path):
         print("\n****************************************\n****************************************\n****************************************\n*** Checkpoint Loading Failed!!!!!!! ***\n****************************************\n****************************************\n****************************************\n")
         return None
 
+
+base_model = EfficientNetB7(weights=os.path.join(os.path.dirname(os.path.abspath(__file__)),'EfficientNet7_Emot.h5'), include_top=False)
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+feature_extractor = Model(inputs=base_model.input, outputs=x)
+
+
 # Function for extraction of features
 def get_features(filename, destination):
     input_string = filename
@@ -56,12 +64,15 @@ def get_features(filename, destination):
         except:
             return None
 
-        base_model = EfficientNetB7(weights=os.path.join(os.path.dirname(os.path.abspath(__file__)),'EfficientNet7_Emot.h5'), include_top=False)
-        x = base_model.output
-        x = GlobalAveragePooling2D()(x)
-        feature_extractor = Model(inputs=base_model.input, outputs=x)
+        # base_model = EfficientNetB7(weights=os.path.join(os.path.dirname(os.path.abspath(__file__)),'EfficientNet7_Emot.h5'), include_top=False)
+        # x = base_model.output
+        # x = GlobalAveragePooling2D()(x)
+        # feature_extractor = Model(inputs=base_model.input, outputs=x)
 
-        features_listofList = []
+        try:
+            features_listofList.clear()
+        except:
+            features_listofList = []
         for indx, frame_file in enumerate(file_paths_frames):
             frame_filename = os.path.join(input_folder, frame_file)
             image = cv2.imread(frame_filename)
